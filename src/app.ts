cddb1849639058a5f17b0664d7b2433ec57c4a65
loqumi <loqumihh@gmail.com>
2022-11-23 21:00:21 +0500

    backend

---
+++ src/app.ts
@@ -0,0 +1,34 @@
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+import * as cors from "cors";
+import "reflect-metadata";
+
+var indexRouter = require("./routes/index");
+
+var app = express();
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+app.use(cors());
+
+app.use("/", indexRouter);
+
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+app.use(function (err, req, res, next) {
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  res.status(err.status || 500);
+  res.render("error");
+});
+
+export default app;
